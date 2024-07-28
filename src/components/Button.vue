aaf12618f71e693aacc929e349dac349da39e551
ㅤㅤㅤㅤㅤ <85992977+whoamizy@users.noreply.github.com>
2024-07-28 16:41:24 +0400

    feat: add components (#1)

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  disabled: false
+})
+</script>
+
+<template>
+  <button
+    class="rounded-md border bg-green-400 px-4 py-2 font-semibold text-black transition hover:bg-green-600 hover:text-white"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
