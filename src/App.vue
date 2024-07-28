8f50526e459848025473f4b57c34d571e5aa488b
whoamizy <delovoysergei098@gmail.com>
2024-07-28 16:40:39 +0400

    feat: add components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex h-screen w-screen flex-col items-center justify-start bg-white pt-10">
+    <div class="grow">
+      <h1 class="mb-4 text-5xl font-bold">Tic-Tac-Toe</h1>
+    </div>
+    <div class="py-3">Made by Whoami</div>
+  </div>
 </template>
---
+++ src/components/Cell.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  value: string
+}
+
+withDefaults(defineProps<Props>(), {
+  value: ''
+})
+</script>
+
+<template>
+  <button
+    class="flex h-16 w-16 items-center justify-center border transition hover:bg-gray-50 disabled:cursor-not-allowed disabled:bg-gray-200"
+    :disabled="!!value"
+    @click="$emit('click')"
+  >
+    {{ value }}
+  </button>
+</template>
